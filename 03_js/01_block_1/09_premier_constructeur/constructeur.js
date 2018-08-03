68b6219bcc403888de4f271ead5cb9da0484440d
owlab-io <contact@owlab.io>
2018-08-03 12:26:49 +0200

    semaine 2, mercredi et jeudi

--- 03_js/01_block_1/09_premier_constructeur/constructeur.js
+++ 03_js/01_block_1/09_premier_constructeur/constructeur.js
@@ -1,21 +1,31 @@
-
-
-var PersonnageMatrix = function(n, g) {
+var PersonnageMatrix = function(n, g, s) {
     this.name = n;
     this.genre = g;
-    console.log(this);
+    // console.log(this);
 };
 
-PersonnageMatrix.faireUneBlague = function () {
-    console.log("j'aurais dû choisir la pillule bleue !!! ><");
+PersonnageMatrix.prototype.sortirArme = function () {
+    console.log(this.name + ": don't move a muscle !!!");
 };
 
-// var badInstance = MonConstruct(); // undefined car on a oublié le mot-clé new ... > <'
-var instance1 = new PersonnageMatrix("neo", "homme"); // {}
+PersonnageMatrix.prototype.faireFeu = function () {
+    console.log(this.name + ": BLAM BLAM BLAM !!!");
+};
+
+PersonnageMatrix.prototype.faireUneBlague = function () {
+    console.log(this.name + ": j'aurais dû choisir la pillule bleue !!! ><");
+};
+
+// var badInstance = MonConstruct();
+// badInstance ci-dessus est undefined car oubli du mot-clé new !
+var instance1 = new PersonnageMatrix("Neo", "homme"); // {}
 var instance2 = new PersonnageMatrix("Trinity", "femme"); // {}
-var instance3 = new PersonnageMatrix("Agent Smith", "machine"); // {}
+var instance3 = new PersonnageMatrix("Smith", "machine"); // {}
+
+instance2.faireUneBlague();
+instance3.sortirArme();
+instance1.faireFeu();
 
 // console.log(instance1);
 // console.log(instance2);
 // console.log(instance2 === instance1);
-// console.log(instance1.faireUneBlague());
