6332267d641e758c15f5a82ec4cb554f838faf1a
owlab-io <contact@owlab.io>
2018-08-27 11:09:11 +0200

    remove node app

--- 03_js/01_block_1/00_hello_world/hello.js
+++
@@ -1,4 +0,0 @@
-/*global window*/
-/*jshint esversion: 6*/
-
-window.console.log("hello world !");
--- 03_js/02_block_2/07_create_color_blocks/app.js
+++
@@ -1,6 +0,0 @@
-/*global window, document*/
-/*jshint esversion:6 */
-var app = (function app() {
-    "use strict";
-    console.log("ready to rock !!!");
-}());
